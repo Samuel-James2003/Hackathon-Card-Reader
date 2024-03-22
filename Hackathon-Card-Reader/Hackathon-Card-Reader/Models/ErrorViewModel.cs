c1e70197b992f7a933c1265f6bd3167fce946993
Samuel James <samueljames.2003@hotmail.com>
2024-03-22 22:39:17 +0100

    re-init merci jason

--- Hackathon-Card-Reader/Hackathon-Card-Reader/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hackathon_Card_Reader.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
