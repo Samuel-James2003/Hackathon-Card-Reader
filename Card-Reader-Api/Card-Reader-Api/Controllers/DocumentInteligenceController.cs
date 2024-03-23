665abf5c73173d9dd50d3c8e90535b00232917fe
Samuel James <samueljames.2003@hotmail.com>
2024-03-23 11:20:17 +0100

    Prepping doc-intel-controller

--- Card-Reader-Api/Card-Reader-Api/Controllers/DocumentInteligenceController.cs
+++ Card-Reader-Api/Card-Reader-Api/Controllers/DocumentInteligenceController.cs
@@ -1,8 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using PokemonTcgSdk.Standard.Features.FilterBuilder.Pokemon;
-using PokemonTcgSdk.Standard.Infrastructure.HttpClients;
-using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Base;
-using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Cards;
 
 namespace Card_Reader_Api.Controllers
 {
@@ -10,6 +6,33 @@ namespace Card_Reader_Api.Controllers
     [Route("[controller]")]
     public class DocumentInteligenceController : ControllerBase
     {
+        /// <summary>
+        /// Gets the details from the card sent
+        /// </summary>
+        /// <returns>A <see cref="string"/> containing the pokemon name and a <seealso cref="string"/> containg the Pokemon ID </returns>
+        [HttpPost(Name = "GetCardDetailsPost")]
+        public async Task<(string,string)> GetCardDetails([FromForm]IFormFile? file, string? url)
+        {
+            if (string.IsNullOrEmpty(url) && file is null)
+                throw new ArgumentNullException(nameof(url));
+            var ext = Path.GetExtension(file.FileName).ToLower();
+            switch (ext)
+            {
+                case "png":
+                case "jpg":
+                    break;
+                case "heif":
+                    throw new NotImplementedException("HEIF not ");
+                default:
+                   throw new ("only takes png and jpg");
+            }
+            string PokeName = "", PokeID = "";
+            await Task.Delay(1);
+
+
+
+            return (PokeName,PokeID);
+        }
 
     }
 }
