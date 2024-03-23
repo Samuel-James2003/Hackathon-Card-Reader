6e85298bd4f86cc4faea3790d60c07b4e36fbac7
MonnoyeRémy <remy.monnoye@gmail.com>
2024-03-23 14:23:30 +0100

    api call

--- Card-Reader-Front/card-reader/src/component/ImportImage.tsx
+++ Card-Reader-Front/card-reader/src/component/ImportImage.tsx
@@ -1,39 +1,60 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const ImageUploader:React.FC =()=>{
-    const [selectedImage,setSelectedImage] = useState<string | null>(null);
+const ImageUploader: React.FC = () => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const [url, setUrl] = useState<string>('');
 
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
-        if(event.target.files && event.target.files[0]){
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
             setSelectedImage(URL.createObjectURL(event.target.files[0]));
         }
-    }; 
+    };
 
-return (
-    <div>
-      <h1>Upload and Display Image using React Hooks and TypeScript</h1>
 
-      {selectedImage && (
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://localhost:5185/DocumentInteligence', {
+                param: url
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
         <div>
-          <img
-            alt="Selected"
-            width={"250px"}
-            src={selectedImage}
-          />
-          <br />
-        </div>
-      )}
+            <h1>Upload and Display Image using React Hooks and TypeScript</h1>
 
-      <br />
-      <br />
-      
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-      />
-    </div>
- );
+            {selectedImage && (
+                <div>
+                    <img
+                        alt="Selected"
+                        width={"250px"}
+                        src={selectedImage}
+                    />
+                    <br />
+                </div>
+            )}
+
+            <br />
+            <br />
+
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+            />
+            <input
+                type="text"
+                placeholder="Enter URL"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+            />
+
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
 };
 
 export default ImageUploader;
