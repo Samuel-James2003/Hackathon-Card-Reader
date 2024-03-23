447fc26935fbb53011bc7c72df6d8fb70f6aee84
JasonChaa <jasoncharlier98@gmail.com>
2024-03-23 10:21:55 +0100

    Front init

--- Card-Reader-Api/Card-Reader-Api/Controllers/DocumentInteligenceController.cs
+++ Card-Reader-Api/Card-Reader-Api/Controllers/DocumentInteligenceController.cs
@@ -11,19 +11,5 @@ namespace Card_Reader_Api.Controllers
     public class DocumentInteligenceController : ControllerBase
     {
 
-
-        [HttpGet(Name = "pokemoncard")]
-        public async Task<ApiResourceList<Card>> Get()
-        {
-            PokemonApiClient pokeClient = new PokemonApiClient("1c32b871-0b3c-4c43-97c3-fcf646079c38");
-            
-            var filter =new Dictionary<string, string>
-            {
-                {"name","Venusaur"},
-                {"number","1"}
-            };
-            var cards = await pokeClient.GetApiResourceAsync<Card>(filter);
-            return cards;
-        }
     }
 }
