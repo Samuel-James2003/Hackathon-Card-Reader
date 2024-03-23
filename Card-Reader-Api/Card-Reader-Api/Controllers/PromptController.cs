3cf5e07cea3c727069528a5ee3d8ed71987098d8
JasonChaa <jasoncharlier98@gmail.com>
2024-03-23 12:18:13 +0100

    chatbot implemented

--- Card-Reader-Api/Card-Reader-Api/Controllers/PromptController.cs
+++ Card-Reader-Api/Card-Reader-Api/Controllers/PromptController.cs
@@ -1,7 +1,9 @@
-﻿using Card_Reader_Api.Model;
+﻿using Azure.AI.OpenAI;
+using Card_Reader_Api.Model;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
+
 
 namespace Card_Reader_Api.Controllers
 {
@@ -9,8 +11,8 @@ namespace Card_Reader_Api.Controllers
     [Route("[controller]")]
     public class PromptController : ControllerBase
     {
-        [HttpGet(Name = "customGpt")]
-        public static async Task<string> AskGpt(string content, string prompt, string system)
+/*        [HttpGet(Name = "customGpt")]
+        public async Task<string> AskGpt(string content, string prompt, string system)
         {
             ChatModel chatModel = new();
             chatModel.AddMessage(
@@ -19,11 +21,52 @@ namespace Card_Reader_Api.Controllers
                 new("user", content));
 
             return await CallOpenAI(chatModel);
+        }*/
+
+        [HttpGet(Name = "test")]
+        public async Task<string> ConversationAnalysis(string prompt, string system)
+        {
+            try
+            {
+                var openai  = new OpenAIClient(new Uri(Env.URL_OPEN_AI), new Azure.AzureKeyCredential(Env.KEY_OPEN_AI));
+
+                string conversationHistory = ""; // Initialize a new string to hold the conversation history
+
+
+                // Append the conversation history to the prompt
+                prompt = $"{prompt}\n {conversationHistory}";
+                ChatRequestSystemMessage systemMessage = new(system) ;
+                ChatRequestUserMessage message = new ChatRequestUserMessage(prompt);
+                
+
+
+                // Paramètres de la requête
+                var requestOptions = new ChatCompletionsOptions
+                {
+                    DeploymentName = "gpt4-003",
+                    Messages = { message,systemMessage },
+                    
+                    
+                };
+
+
+                // Envoyer la requête à Azure OpenAI
+                var response = await openai.GetChatCompletionsAsync(requestOptions);
+                // Extraire la réponse
+                var analysisResult = response.Value.Choices.FirstOrDefault()?.Message?.Content;
+                return analysisResult ?? "Aucune réponse n'a été reçue.";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
-            public static async Task<string> CallOpenAI(ChatModel messages)
+
+        /*public static async Task<string> CallOpenAI(ChatModel messages)
             {
-                var jsonPayloadBuilder = new JsonPayloadBuilder("gpt-35-turbo");
+            var jsonPayloadBuilder = new JsonPayloadBuilder("gpt4-003");
                 messages.Messages.ForEach(chatModel => { jsonPayloadBuilder.AddMessage(chatModel.Role, chatModel.Content); });
                 string jsonPayload = jsonPayloadBuilder.BuildPayload();
 
@@ -53,13 +96,13 @@ namespace Card_Reader_Api.Controllers
                         if (firstChoice != null)
                         {
                             return firstChoice.message.content;
-                        }
                     }
+                }
 
                     // Handling request errors
                     return response.StatusCode.ToString();
                 }
-            }
+            }*/
     }
  }
 
