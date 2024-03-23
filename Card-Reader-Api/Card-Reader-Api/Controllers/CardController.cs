12c98c85d229908ba484f241edf5d8b063152fde
Samuel James <samueljames.2003@hotmail.com>
2024-03-23 10:44:16 +0100

    GetPokemonCArd

--- Card-Reader-Api/Card-Reader-Api/Controllers/CardController.cs
+++ Card-Reader-Api/Card-Reader-Api/Controllers/CardController.cs
@@ -5,22 +5,31 @@ using PokemonTcgSdk.Standard.Infrastructure.HttpClients;
 
 namespace Card_Reader_Api.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class CardController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Pokemon name</param>
+        /// <param name="number"> Pokemon Identification Number</param>
+        /// <returns> <see cref="Card"/> that matches the Name and ID  </returns>
+        [HttpGet(Name = "GetPokemonCard")]
+        public async Task<Card> GetPokemonCard([FromQuery] string name, [FromQuery] string number)
         {
-          [HttpGet(Name = "pokemoncard")]
-            public async Task<ApiResourceList<Card>> Get([FromQuery] string name, [FromQuery] string number)
-            {
-                PokemonApiClient pokeClient = new PokemonApiClient(Env.KEY_POKEMON);
+            PokemonApiClient pokeClient = new PokemonApiClient(Env.KEY_POKEMON);
 
-                var filter = new Dictionary<string, string>
+            var filter = new Dictionary<string, string>
             {
                 {"name",name},
                 {"number",number}
             };
-                var cards = await pokeClient.GetApiResourceAsync<Card>(filter);
-                return cards;
-            }
+            var cards = await pokeClient.GetApiResourceAsync<Card>(filter);
+            return cards.Results.First();
         }
+    }
 }
